81e9f32c291d66c28e11432986493fa37c659df6
DANIEL DA SILVA CALADO <20161d13gr0031@LABS.GARANHUNS.IFPE>
2018-04-18 20:28:39 -0300

    Criação da classe Jogador

---
+++ Jogador.lua
@@ -0,0 +1,15 @@
+jogador={}
+
+function jogador:criarJogador()
+	novoJogador = {nome = " ", jogada = " "}
+
+	return novoJogador
+end
+
+jogador1 = jogador:criarJogador()
+jogador2 = jogador:criarJogador()
+
+	jogador2.jogada = "O"
+	jogador1.jogada = "x"
+
+
