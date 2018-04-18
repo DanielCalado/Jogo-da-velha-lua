f42a1069b8a07d60752d7f335b5c143c83dbb204
DANIEL DA SILVA CALADO <20161d13gr0031@LABS.GARANHUNS.IFPE>
2018-04-18 20:30:55 -0300

    atualização do jogo

--- Tabuleiro.lua
+++ Tabuleiro.lua
@@ -6,7 +6,7 @@ function tabuleiro:gerar()
 		tabuleiro[linha] = {}
 
 		for coluna = 1, 3 do
-			tabuleiro[linha][coluna]= " "
+			tabuleiro[linha][coluna]= 1
 		end
 	end
 	return tabuleiro
@@ -19,10 +19,17 @@ function tabuleiro:visualizar()
 			for coluna = 1, 3 do
 
 				if coluna ~= 3 then
-					aux = aux .. tabuleiro[linha][coluna].." | "
+					if(tabuleiro[linha][coluna] == 1)then
+						aux = aux .. " ".." | "
+					else
+						aux = aux .. tabuleiro[linha][coluna].." | "
+					end
 				else
-					aux = aux .. tabuleiro[linha][coluna]
-
+					if(tabuleiro[linha][coluna] == 1)then
+						aux = aux .. " "
+					else
+						aux = aux .. tabuleiro[linha][coluna]
+					end
 				end
 			end
 			print(aux)
@@ -32,67 +39,55 @@ function tabuleiro:visualizar()
 		end
 end
 
-tabuleiro:gerar()
-------------------------------------------------------------------
-------------------------------------------------------------------
 
 
-jogador={}
-
-function jogador:criarJogador()
-	novoJogador = {nome = " ", jogada = " ", pontos = 0}
-
-	return novoJogador
-end
-
-jogador1 = jogador:criarJogador()
-jogador2 = jogador:criarJogador()
-
-	jogador2.jogada = "O"
-	jogador1.jogada = "x"
-
-
-------------------------------------------------------------------
-------------------------------------------------------------------
 
 function tabuleiro:verificarGanhouX()
 	if(tabuleiro[1][1] == "X" and tabuleiro[1][2] == "X" and tabuleiro[1][3] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[2][1] == "X" and tabuleiro[2][2] == "X" and tabuleiro[2][3] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 
 		i = 10
 	end
 	if(tabuleiro[3][1] == "X" and tabuleiro[3][2] == "X" and tabuleiro[3][3] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][1] == "X" and tabuleiro[2][1] == "X" and tabuleiro[3][1] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][2] == "X" and tabuleiro[2][2] == "X" and tabuleiro[3][2] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][3] == "X" and tabuleiro[2][3] == "X" and tabuleiro[3][3] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][1] == "X" and tabuleiro[2][2] == "X" and tabuleiro[3][3] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][3] == "X" and tabuleiro[2][2] == "X" and tabuleiro[3][1] == "X")then
+		print("..........................")
 		print("Jogador(X) " .. jogador1.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
@@ -102,42 +97,50 @@ end
 
 function tabuleiro:verificarGanhouO()
 	if(tabuleiro[1][1] == "O" and tabuleiro[1][2] == "O" and tabuleiro[1][3] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[2][1] == "O" and tabuleiro[2][2] == "O" and tabuleiro[2][3] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[3][1] == "O" and tabuleiro[3][2] == "O" and tabuleiro[3][3] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][1] == "O" and tabuleiro[2][1] == "O" and tabuleiro[3][1] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][2] == "O" and tabuleiro[2][2] == "O" and tabuleiro[3][2] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][3] == "O" and tabuleiro[2][3] == "O" and tabuleiro[3][3] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][1] == "O" and tabuleiro[2][2] == "O" and tabuleiro[3][3] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
 	if(tabuleiro[1][3] == "O" and tabuleiro[2][2] == "O" and tabuleiro[3][1] == "O")then
-		print("Jogador(X) " .. jogador2.nome .. " Ganhou")
+		print("..........................")
+		print("Jogador(O) " .. jogador2.nome .. " Ganhou")
 		reiniarJogo()
 		i = 10
 	end
@@ -145,81 +148,76 @@ function tabuleiro:verificarGanhouO()
 end
 
 function tabuleiro:enpatou()
-	if(i == 10)then
+		print("..........................")
 		print("Jogo enpatado")
 		reiniarJogo()
-	end
+
 end
 
-
 function tabuleiro:iniciarJogadas()
-	i = 1
-	tabuleiro:enpatou()
+i = 1
 	while i <= 9 do
+
 		if(i % 2 == 0)then
-			print("Jogador" .. jogador1.nome .. " jogue:(X)")
+			print("..........................")
+			print("Jogador " .. jogador1.nome .. " jogue:(X)")
+				print("..........................")
 				print("digite a linha:")
-					linhaLocal = io.read('*number')
+					linhaLocal = io.read("*number")
+
+				print("..........................")
 				print("digite a coluna:")
-					colunaLocal = io.read('*number')
-						if(tabuleiro[linhaLocal][colunaLocal] == " ")then
+					colunaLocal = io.read("*number")
+
+					if(type(linhaLocal) == "number" and type(colunaLocal) == "number")then
+						if(tabuleiro[linhaLocal][colunaLocal] == 1)then
 							tabuleiro[linhaLocal][colunaLocal] = "X"
 							tabuleiro:visualizar()
 							i = i + 1
 						tabuleiro:verificarGanhouX()
 						else
+							print("..........................")
 							print("Jogada invalida ou espaço ja ocupado, jogue novamente!!!")
 						end
+					else
+						print("..........................")
+						print("Você digitou uma letra, Digite um numero!!")
+						io.read(1)
+					end
+
 
 		else
-			print("Jogador" .. jogador1.nome .. " jogue:(O)")
+			print("..........................")
+			print("Jogador " .. jogador1.nome .. " jogue:(O)")
+				print("..........................")
 				print("digite a linha:")
 					linhaLocal = io.read('*number')
+				print("..........................")
 				print("digite a coluna:")
 					colunaLocal = io.read('*number')
-
-					if(tabuleiro[linhaLocal][colunaLocal] == " ")then
+						if(type(linhaLocal) == "number" and type(colunaLocal) == "number")then
+						if(tabuleiro[linhaLocal][colunaLocal] == 1)then
 							tabuleiro[linhaLocal][colunaLocal] = "O"
 							tabuleiro:visualizar()
 							i = i + 1
 							tabuleiro:verificarGanhouO()
-					else
-						print("Jogada invalida ou espaço ja ocupado, jogue novamente!!!")
+						else
+							print("..........................")
+							print("**Digite um numero entre 1 e 3** \n**Escolha uma casa vazia**")
+						end
+
+						else
+							print("..........................")
+							print("Você digitou uma letra, Digite um numero!!")
+							io.read(1)
 						end
 		end
+
+	end
+
+	if(i == 10)then
+		tabuleiro:enpatou()
 	end
 end
 
-function jogaDaVelha()
-	print("Bem vindo ao joga da velha")
-	print("..........................")
-	print("Jogador O digite seu nome:")
-		jogador1.nome = io.read()
-	print("Jogador X digite seu nome:")
-		jogador2.nome = io.read()
 
-end
-	reiniciar = " "
-function reiniarJogo()
-
-
-
-	print("Obrigado por jogar nosso jogo!!")
-	reiniciar = io.read()
-	print("Deseja jogar novamente?(sim ou não)")
-		reiniciar = io.read()
-
-	if(reiniciar == "sim")then
-		tabuleiro:gerar()
-		tabuleiro:visualizar()
-		tabuleiro:iniciarJogadas()
-	else
-		print("Obrigado por jogar")
-	end
-end
-
-jogaDaVelha()
-
-tabuleiro:visualizar()
-
-tabuleiro:iniciarJogadas()
